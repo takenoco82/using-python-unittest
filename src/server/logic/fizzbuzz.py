84e26dde272a8ac5b3dfa887ca063470f371023d
Daisuke TAKEUCHI <takenoco82@gmail.com>
2018-12-23 15:19:29 +0900

    [fix] fizzbuzzのロジックを実装

--- src/server/logic/fizzbuzz.py
+++ src/server/logic/fizzbuzz.py
@@ -3,4 +3,11 @@ def fizzbuzz_gen(n: int):
 
 
 def fizzbuzz(n: int) -> str:
-    return str(n)
+    if n % 15 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
